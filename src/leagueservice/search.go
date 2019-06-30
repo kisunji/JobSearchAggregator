0ea8ad3c93c0ae42ee22732a19157e825c02e7c5
Chris Sungjin Kim <kisunji92@gmail.com>
2019-06-29 21:12:00 -0400

    Parse html for fields required for Job struct

--- src/leagueservice/search.go
+++ src/leagueservice/search.go
@@ -9,34 +9,19 @@ import (
 
 const leagueURL = "https://league.com/ca/careers-at-league/jobs/"
 
-// Job holds a subset of fields I care about
-type Job struct {
-	Title                   string
-	Qualifications          string `json:"basic_qualifications"`
-	PreferredQualifications string `json:"preferred_qualifications"`
-	DatePosted              string `json:"posted_date"`
-	Category                string `json:"business_category"`
-	URL                     string `json:"job_path"`
-	TimeSinceLastUpdated    string `json:"updated_time"`
-}
-
-// JobList represents the highest level struct returned by Amazon API
-type JobList struct {
-	Jobs []Job
-}
-
 // GetSearchResults does something
 func GetSearchResults() {
 	doc := getDocNode(leagueURL)
 	results := doc.Find(".job-openings__container__jobs__job").
-		FilterFunction(func(i int, s *goquery.Selection) bool {
-			for _, v := range s.Children().Nodes {
-				if v.Data == "h4" && v.FirstChild.Data == "Engineering" {
-					return true
+		FilterFunction(
+			func(i int, s *goquery.Selection) bool {
+				for _, v := range s.Children().Nodes {
+					if v.Data == "h4" && v.FirstChild.Data == "Engineering" {
+						return true
+					}
 				}
-			}
-			return false
-		})
+				return false
+			})
 	jobsContainer := results.Find(".job-openings__container__jobs--list")
 	filteredJobs := jobsContainer.Children().FilterFunction(func(i int, s *goquery.Selection) bool {
 		return !strings.Contains(s.Text(), "Director") && !strings.Contains(s.Text(), "Senior")
@@ -61,6 +46,33 @@ func getDocNode(URL string) *goquery.Document {
 
 func getJobPosting(url string) {
 	doc := getDocNode(url)
-	results := doc.Find("div.section.page-centered").Text()
-	log.Print(results)
+	titleSelection := doc.Find("meta").FilterFunction(
+		func(i int, s *goquery.Selection) bool {
+			v, _ := s.Attr("property")
+			return v == "og:title"
+		})
+	title, ok := titleSelection.Attr("content")
+	if !ok {
+		log.Print("No title found")
+		return
+	}
+	log.Print(title)
+	descriptionSelection := doc.Find("meta").FilterFunction(
+		func(i int, s *goquery.Selection) bool {
+			v, _ := s.Attr("property")
+			return v == "og:description"
+		})
+	description, ok := descriptionSelection.Attr("content")
+	if !ok {
+		log.Print("No description found")
+	}
+	log.Print(description)
+	requirementSelection := doc.Find("ul.posting-requirements.plain-list > ul > li")
+	var requirementArr []string
+	for _, n := range requirementSelection.Nodes {
+		requirementArr = append(requirementArr, n.FirstChild.Data)
+	}
+	requirements := strings.Join(requirementArr, "<br/>")
+	log.Print(requirements)
+
 }
--- src/main.go
+++ src/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/kisunji/jobsearchaggregator/src/amazonservice"
 	"github.com/kisunji/jobsearchaggregator/src/leagueservice"
 )
 
 func main() {
 	leagueservice.GetSearchResults()
-	//amazonservice.GetSearchResults()
+	amazonservice.GetSearchResults()
 }
