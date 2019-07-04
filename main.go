78be7b72e12dba9060030f2929a65a42fde9b30a
Chris Sungjin Kim <kisunji92@gmail.com>
2019-07-03 22:56:47 -0400

    Wrap service calls with goroutines

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-
 	"github.com/kisunji/jobsearchaggregator/jobservice"
 )
 
@@ -17,13 +16,19 @@ var (
 	ErrJobService = errors.New("There was an issue with the jobservice API")
 )
 
-// Handler is the AWS Lambda function handler that uses Amazon API Gateway request/response
+//Handler is the AWS Lambda function handler that uses Amazon API Gateway request/response
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	defer timeTrack(time.Now(), "Handler")
 	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+	c := make(chan []jobservice.Job)
+	go func() { c <- jobservice.AmazonJobs() }()
+	go func() { c <- jobservice.LeagueJobs() }()
 	var jobArray []jobservice.Job
-	jobArray = append(jobArray, jobservice.AmazonJobs()...)
-	jobArray = append(jobArray, jobservice.LeagueJobs()...)
+	for i := 0; i < 2; i++ {
+		result := <-c
+		jobArray = append(jobArray, result...)
+	}
+	log.Printf("Jobs found: %d", len(jobArray))
 	bytes, err := json.Marshal(jobArray)
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, ErrJobService
@@ -39,7 +44,7 @@ func main() {
 }
 
 // timeTrack measures time to execute
-// Taken from https://blog.stathat.com/2012/10/10/time_any_function_in_go.html
+// Credits: https://blog.stathat.com/2012/10/10/time_any_function_in_go.html
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
 	log.Printf("%s took %s", name, elapsed)
