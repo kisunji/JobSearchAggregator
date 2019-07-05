91c744a933e4abcb9b7779b96fcbfd1462679c53
Chris Sungjin Kim <kisunji92@gmail.com>
2019-07-04 20:31:01 -0400

    Add missing return

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func LocalHandler(w http.ResponseWriter, req *http.Request) {
 	bytes, err := getJobs()
 	if err != nil {
 		http.Error(w, "Error occurred", http.StatusInternalServerError)
+		return
 	}
 	w.Write(bytes)
 }
